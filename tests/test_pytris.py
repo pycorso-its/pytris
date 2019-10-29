61c27c1ac812596986f99907f1f2303196c0d4ac
juancjuanc <57158591+juancjuanc@users.noreply.github.com>
2019-10-29 20:08:36 -0300

    Update test_pytris.py

--- tests/test_pytris.py
+++ tests/test_pytris.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 
-
 class SampleTestCase(TestCase):
     def test_pass(self):
-        self.assertTrue("dummy sample test")
+        self.assertTrue("dummy sample test.")
